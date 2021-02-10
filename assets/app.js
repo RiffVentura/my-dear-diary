768ddce836e9f4978730b7261f5960c3dd009833
Riff Ventura <riff.ventura@gmail.com>
2021-02-10 20:34:14 +0100

    added uikit to the project

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,12 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 
-console.log('yooo yooo')
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification('Hello world.');
+
+console.log('test')
